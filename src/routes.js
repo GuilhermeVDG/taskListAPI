a718d453ebe9a1de26cdfb46ab5de9beaac32411
Guilherme Vanderley <guivdouradog@gmail.com>
2022-09-08 21:48:36 -0300

    created migration

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/teste", (req, res) => res.json({ ok: true }));
+routes.get('/teste', (req, res) => res.json({ ok: true }));
 
 export default routes;
