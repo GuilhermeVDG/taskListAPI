610af85f8d3f1c49ab652cac2349ce7d21389eba
Guilherme Vanderley <guivdouradog@gmail.com>
2022-09-11 21:53:54 -0300

    fixed user registration

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/teste', async (req, res) => {
-  const user = await User.create({
-    name: 'Guilherme',
-    email: 'guilherme@gmail.com',
-    password_hash: '1234567',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
