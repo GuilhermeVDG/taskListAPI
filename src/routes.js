3dcc96ecf1e1364e1457e07b82aae3517c0880a2
Guilherme Vanderley <guivdouradog@gmail.com>
2022-09-11 21:39:07 -0300

    loaded models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ ok: true }));
+routes.get('/teste', async (req, res) => {
+  const user = await User.create({
+    name: 'Guilherme',
+    email: 'guilherme@gmail.com',
+    password_hash: '1234567',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
