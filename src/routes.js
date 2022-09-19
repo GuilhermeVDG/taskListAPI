01b2845549d78284c34f24f1bd989f7ff5ac25af
Guilherme Vanderley <guivdouradog@gmail.com>
2022-09-19 17:11:55 -0300

    feat(tasks): add store and list

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import TaskController from './app/controllers/TaskController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -13,5 +14,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/tasks', TaskController.store);
+routes.get('/tasks', TaskController.list);
 
 export default routes;
